a642154d718ff98eb7ce316c3d798391727e9749
Emmanuel <0.emmanuelpopoola@gmail.com>
2023-05-28 12:21:18 +0100

    Add eslint️

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,15 +3,38 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-typescript/recommended', '@vue/eslint-config-prettier', 'prettier'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
+    'prettier',
+    '@vue/typescript',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+  ],
   overrides: [
     {
-      files: ['cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}'],
-      extends: ['plugin:cypress/recommended'],
+      files: ['*.vue', '*.ts'],
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+      plugins: ['@typescript-eslint'],
+      extends: ['plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-recommended'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        'prettier/prettier': 'error',
+        'vue/max-attributes-per-line': 'off',
+        'vue/html-self-closing': 'off',
+        'vue/singleline-html-element-content-newline': 'off',
+      },
     },
   ],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     'prettier/prettier': 'error',
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
   },
 };
